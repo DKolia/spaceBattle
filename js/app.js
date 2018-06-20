bb3fc01457fa7dabe4fe4e3eb6169ae5ceccb372
David Kolia <Dkolia@icloud.com>
2018-06-20 17:16:46 -0500

    Began and pseudocoded a Space Battle Game

---
+++ js/app.js
@@ -0,0 +1,85 @@
+console.log("Hello Aliens");
+accuracy is the chance between 0 and 1 that the ship will hit its target.
+// Is 0 and 1 set values in Javascript, or is this something we need to define ourselves?
+
+//  Protagonist Space Ship Stats
+const ussAssembly = {
+    this.hull: 20,             // The "Health" of the ship
+    this.firepower: 5,         // The amount of damage inflicted
+    this.accuracy: .7,        // The odds of accuracy for each attack
+
+    attempt accuracy check.    // The main attack method
+    attackAliens(alie )  {
+
+      If success
+        remove from [opponent] hull value 5.
+        console.log("It's a hit!");
+        if Alien_Space_Ship hull <= 0;
+          console.log("Alien_Space_Ship explodes!");
+      If failure
+        console.log("The USS Assembly missed...");
+    }
+}
+
+//  Alien Space Ship Stats
+Class AlienSpaceShip {
+  constructor(){
+    this.hull: between 3 and 6          // The range of "Health" for the aliens attacking
+    this.firepower: between 2 and 4     // The range of damage inflicted
+    this.accuracy: between .6 and .8    // The range of accuracy for each attack
+    }
+    attackUssAssembly() { attempt accuracy check.   // The main attack method
+      if success
+      remove from USS_Assembly hull value 5.
+      if USS_Assembly hull <= 0
+        console.log("The USS Assembly is struck and explodes!");
+        run end-game function.
+      console.log("the USS Assembly is struck!");
+      if failure
+      console.log("the alien ship missed!");
+        }
+      }
+
+//
+Spawn Alien_Space_Ship(Stat1, stat2, stat3) {
+  console.log("A new enemy has entered the fray!");
+}
+
+// Game logic
+const game = {
+  timer: 100;
+  alienShips: [],
+  spawn() {
+
+  }
+}
+
+
+const ship = new AlienSpaceShip(asdf, asdf, asdf);
+this.alienShips.push(ship)
+//  Attack loop
+    attackAliens method
+  if Alien_Space_Ship is hit, resolve damage
+    console.log("Alien ship hit and recieves damage");
+  if alien is survives,
+    console.log("Alien ship retaliates!");
+    attackUssAssembly method
+
+
+//  Retreat Option
+console.log("You flee to regain the high ground!");
+run end-Game
+
+
+//  End Game
+end-Game
+  console.log("The day is lost. Reload the page to try again!");
+
+
+
+
+You could be battling six alien ships each with unique values.
+
+
+Example use of accuracy to determine a hit:
+if (Math.random() < alien[0].accuracy) {
